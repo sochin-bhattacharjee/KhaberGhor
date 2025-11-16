60fd5ec4077826d58b706db229a5dd128025cbaf
sochin-bhattacharjee <sochin.cs@gmail.com>
2025-11-16 08:44:34 +0600

    I connected Express server to Mongodb through Mongoose.

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,11 @@
 import express from "express"
-
+import dotenv from "dotenv"
+import connectDB from "./config/db.js";
+dotenv.config()
 const app = express()
+const port = process.env.PORT || 3000;
+
+app.listen(port, ()=>{
+    connectDB()
+    console.log(`Khaber Ghor server is running at ${port}`)
+})
